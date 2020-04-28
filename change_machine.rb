3ac3791bb70d42294bbad90ed36982ebab1055b9
ernestomelchor <nurturelove88@gmail.com>
2020-04-27 19:20:54 -0500

    add problem to solve using test driven development (tdd)

---
+++ change_machine.rb
@@ -0,0 +1,2 @@
+# Exercise:
+# You will be creating a method that will take a number of cents and return an array of coins that will be your change. The goal is to return as few coins as possible. Assume that the highest denomination of coins will be a quarter (25 cents). For example: If the method receives 119, the result will be [25, 25, 25, 25, 10, 5, 1, 1, 1, 1]
